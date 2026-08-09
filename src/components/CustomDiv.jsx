d40f71ae6b4155deb7ea007131c12188ee65a870
phe0nix <starlordindia1230@gmail.com>
2026-08-09 14:12:09 +0530

    added responsiveness for why us section

--- src/components/CustomDiv.jsx
+++ src/components/CustomDiv.jsx
@@ -7,7 +7,7 @@ const CustomDiv = () => {
     <section className="relative min-h-screen overflow-hidden bg-[#f8f1e7]">
 
             {/* FLOWER IMAGE */}
-            <div className="absolute inset-y-0 left-0 w-[35%]">
+            <div className="absolute inset-y-0 left-0 w-1/2 lg:w-[35%] max-md:hidden">
                 <img
                     src="../../images/why-bg.png"
                     alt="Anom Flora"
@@ -18,7 +18,7 @@ const CustomDiv = () => {
 
             {/* WHITE / CREAM CURVED PANEL */}
             <svg
-                className="pointer-events-none absolute inset-0 z-10 h-full w-full"
+                className="pointer-events-none absolute inset-0 z-10 h-full w-full max-md:hidden"
                 viewBox="0 0 100 100"
                 preserveAspectRatio="none"
             >
@@ -67,10 +67,10 @@ const CustomDiv = () => {
 
 
             {/* CONTENT */}
-            <div className="relative z-20 ml-[35%] flex min-h-screen pr-16 py-25 w-[65%]">
+            <div className="relative md:absolute lg:relative z-20 lg:ml-[35%] flex min-h-screen w-full lg:pr-16 lg:py-25 lg:w-[65%] px-5 md:px-10 md:py-15 py-10">
 
                 <div ref={headerRef} className="mb-10 w-full">
-                    <div className="flex items-center justify-start gap-3 mb-3 pl-38">
+                    <div className="flex items-center justify-center lg:justify-start gap-3 mb-3 lg:pl-38">
                         <img src="../../icon/leaf-icon.png" className='w-8 -scale-x-90' alt="" />
                         <p className="text-xs tracking-[0.25em] uppercase font-semibold" style={{ color: '#C08D34' }}>
                             Why Anom Flora?
@@ -78,32 +78,32 @@ const CustomDiv = () => {
                         <img src="../../icon/leaf-icon.png" className='w-8' alt="" />
                     </div>
 
-                    <h2 className="font-elegant text-start text-4xl md:text-5xl lg:text-6xl font-medium text-primary leading-tight">
+                    <h2 className="font-elegant text-center lg:text-start text-4xl md:text-5xl lg:text-6xl font-medium text-primary leading-tight">
                         More Than Just Flowers,
                     </h2>
-                    <h2 className="font-script text-4xl md:text-5xl lg:text-6xl leading-tight mt-1 lg:pl-5 text-prime-yellow">
+                    <h2 className="font-script text-4xl max-lg:text-center md:text-5xl lg:text-6xl leading-tight mt-1 lg:pl-5 text-prime-yellow">
                         Its Our Promise 
                     </h2>
-                    <div className="flex items-center justify-start gap-3 mt-5 pl-50">
+                    <div className="flex items-center justify-center lg:justify-start gap-3 mt-5 lg:pl-50">
                         <div className="h-px w-16 bg-linear-to-r from-transparent to-primary"></div>
                         <img src="../../icon/patel-leafs.png" className='w-6' alt="" />
                         <div className="h-px w-16 bg-linear-to-l from-transparent to-primary" ></div>
                     </div>
 
-                    <p className="text-warm-gray text-sm md:text-base mt-4 max-w-xl leading-relaxed text-center pl-8">
+                    <p className="text-warm-gray text-sm md:text-base mt-4 max-w-md max-lg:mx-auto lg:max-w-xl leading-relaxed text-center lg:pl-8 max-md:px-10">
                         At Anom Flora, we believe in creating everlasting beauty with uncompromising quality and a passion for perfection.
                     </p>
 
                     {/* Ornamental divider */}
-                    <div className="w-full flex justify-center mt-8 flex-wrap gap-6 ">
+                    <div className="w-full flex justify-center mt-8 flex-wrap gap-3 md:gap-6 ">
                         { features.map((feature, index) => (
-                            <div className='w-[31%] h-fit bg-white/40 py-10 flex flex-col items-center rounded-2xl group  hover:bg-white backdrop-blur-lg transition-smooth'>
+                            <div className='w-[45%] md:w-[31%] h-fit bg-white/40 py-10 flex flex-col items-center rounded-2xl group  hover:bg-white lg:backdrop-blur-lg transition-smooth backdrop-blur-sm max-md:px-3 break-inside-avoid'>
                                 <div className='p-3 rounded-full bg-[#fce7d2] overflow-hidden group-hover:bg-[#feddae] transition-smooth'>
-                                    <img src={feature.icon} className='size-12 group-hover:scale-115 transition-smooth' alt="" />
+                                    <img src={feature.icon} className='size-8 md:size-12 group-hover:scale-115 transition-smooth' alt="" />
                                 </div>
-                                <h3 className='text-base font-semibold font-sans mt-5 text-primary '>{feature.title}</h3>
+                                <h3 className='text-base font-semibold font-sans mt-5 text-primary text-center'>{feature.title}</h3>
 
-                                <p className='text-xs text-center font-sans font-medium mt-2 text-warm-gray px-10'>{feature.description}</p>
+                                <p className='text-xs text-center font-sans font-medium mt-2 text-black/60 lg:text-warm-gray md:px-5 lg:px-10'>{feature.description}</p>
                                 <div className='w-10 h-0.5 rounded bg-prime-yellow mt-5'></div>
                             </div>
                         ))}
